77111a8ff51f6cc3c8dbd0d4121db381901a1af6
Mariam Anuashvili <m.anuashvili@gmail.com>
2019-11-24 00:45:18 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -5,16 +5,4 @@ def display_rainbow(colors)
   puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]},"
 end
 
-
-
-
-bob = ["Bob", 46, "Juggler", "555-555-5555"]
-stefani = ["Stefani", 49, "Firefighter", "555-111-1111"]
-
-def print_business_card(person)
-  puts "Name: #{person[0]}, Age: #{person[1]}, Occupation: #{person[2]}"
-  puts "Contact: #{person[3]}"
-end
- 
-print_business_card(bob)
-print_business_card(stefani) 
+print display_rainbow(colors)
